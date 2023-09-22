93120a6959f1de39f571a889b27e2c77eeee61a7
Nick Murry <86535318+ridley88@users.noreply.github.com>
2023-09-21 17:36:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let messageInput = document.getElementById("message-input");
+
+messageInput.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    getMessage();
+  }
+});
+
+function getMessage() {
+  document.getElementById("message-output").innerHTML = messageInput.value;
+  messageInput.value = "";
+}
+
+function testing() {
+  console.log("testing worked!");
+}
